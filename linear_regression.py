7ef2345a6ad3c31152c511c5f6fe0d26a0adef86
wasimcreates@gmail.com <fiyee531>
2019-01-20 14:48:17 -0700

    Vander matrix

--- linear_regression.py
+++ linear_regression.py
@@ -8,6 +8,19 @@ import numpy as np
 np.random.seed(3)
 
 
+def vander_matrix(X, degree=4):
+    assert degree > 0
+    assert len(X) > 0
+
+    X = np.asarray(X)
+    order = degree + 1
+    v = np.empty((len(X), order))
+    v[:, degree] = 1
+    for i in range(0, degree):
+        v[:, i] = X ** (degree - i)
+    return v
+
+
 def generate_data():
     N = [15, 100]
     sigma = [0.01, 0.05, 0.2]
@@ -18,27 +31,24 @@ def generate_data():
         for sigma_value in sigma:
             noise = np.random.normal(0, sigma_value, num_points)
             x = np.random.uniform(0, 3, num_points)
-            # y = x ** 2 - 3 * x + 1  # Compute y from x
-
-            blank = np.ones((num_points, 2))
-            blank[:, 1] = x
-            x = blank
-            w = np.array([3, 2])  # y = 2x + 3
-            y = np.dot(x, w) + noise  # y = 2x + 3 + noise
+            y = x ** 2 - 3 * x + 1 + noise  # Compute y from x
 
             gen_data.append((x, y))
     return gen_data
 
 
-def linear_regression(X, y):
+def linear_regression(X, y, degree=9):
+    X = vander_matrix(X, degree)
     xtx_inverse = np.linalg.inv(np.dot(X.transpose(), X))
     xty = np.dot(X.transpose(), y)
     weights = xtx_inverse.dot(xty)
-    print(weights)
+
+    print(np.round(weights, 3))
     return weights
 
 
-def linear_regression_inplace(X, y):
+def linear_regression_inplace(X, y, degree=2):
+    X = vander_matrix(X, degree)
     weights = np.linalg.inv(np.dot(X.transpose(), X)).dot(np.dot(X.transpose(), y))
     return weights
 
@@ -61,6 +71,8 @@ if __name__ == '__main__':
     weights = []
     for [X, y] in data:
         weights.append(linear_regression(X, y))
+        # weights.append(linear_regression_inplace(X, y))
+        # break
 
-    for weight, [X, y] in zip(weights, data):
-        check_fit(X, y, X, y, weight)
+    # for weight, [X, y] in zip(weights, data):
+    #     check_fit(X, y, X, y, weight)
