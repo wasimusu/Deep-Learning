237377742fb4ffb2921e3eab9609684b2867c20f
wasimcreates@gmail.com <fiyee531>
2019-02-03 01:44:51 -0700

    Simpler regression py file

--- HW2/prob_1_ridge_regression.py
+++ HW2/prob_1_ridge_regression.py
@@ -1,6 +1,6 @@
 """
 Generating data and adding gaussian noise to it.
-And applying linear regression to it.
+And applying ridge regression to it.
 """
 
 import numpy as np
@@ -32,125 +32,102 @@ def vander_matrix(X, degree=4):
 
 
 def generate_data(N, sigma):
-    gen_data = []  # Generated data container
-
-    for num_points in N:
-        for sigma_value in sigma:
-            noise = np.random.normal(0, sigma_value, num_points)
-            x = np.random.uniform(0, 3, num_points)
-            y = x ** 2 - 3 * x + 1 + noise  # Compute y from x
-
-            gen_data.append((x, y))
-
-    return gen_data
+    """ Generate data with given number of points N and sigma """
+    noise = np.random.normal(0, sigma, N)
+    X = np.random.uniform(0, 3, N)
+    Y = X ** 2 - 3 * X + 1 + noise  # Compute y from x
+    return X, Y
 
 
 def linear_regression(X, y, degree=2):
+    """ Fit a linear regression equation of given degree to X, Y """
     XV = vander_matrix(X, degree)
     coeffs = np.linalg.inv(np.dot(XV.transpose(), XV)).dot(np.dot(XV.transpose(), y))
-    fit_y = fit(X, coeffs)
+    fit_y = predict(X, coeffs)
     mse = np.sum((fit_y - y) ** 2)
     return coeffs, mse
 
 
 def ridge_regression(X, Y, degree=2, lambda_p=0):
     """
+    Fit a ridge regression equation of given degree to X, Y
     :param lambda_p : regularization parameter
     :param degree : degree of polynomial fit
     """
     XV = vander_matrix(X, degree)
     XVTXV = np.dot(XV.transpose(), XV)
     coeffs = np.linalg.inv(XVTXV + lambda_p * np.eye(XVTXV.shape[0])).dot(np.dot(XV.transpose(), Y))
-    fit_y = fit(X, coeffs)
+    fit_y = predict(X, coeffs)
     mse = np.sum((fit_y - Y) ** 2)
     return coeffs, mse
 
 
-def fit(X, coeff):
+def predict(X, coeff):
     """ Fit X to the coefficients computed after fitting polynomial"""
     X = vander_matrix(X, len(coeff) - 1)
     Y = np.dot(X, coeff)
     return Y
 
 
-def check_fit(train_X, train_y, test_X, test_y, coeff):
-    train_y_fitted = fit(train_X, coeff)
-    test_y_fitted = fit(test_X, coeff)
-
+def cross_validate(X, y, coeff, sigma):
+    """
+    Algorithm :
+    - Generate test data according to the given sigma
+    - Compute the mse for test data
+    - if test_mse > train_mse : overfit else underfit
+    """
+    y_fitted = predict(X, coeff)
     # The errors has been normalized to length so that lengthy vectors do not have higher errors
-    train_error = np.sum((train_y - train_y_fitted) ** 2) / len(train_X)
+    train_error = np.sum((y - y_fitted) ** 2) / len(X)
+
+    test_X, test_y = generate_data(300, sigma)
+    test_y_fitted = predict(test_X, coeff)
     test_error = np.sum((test_y - test_y_fitted) ** 2) / len(test_y)
 
-    model = "Overfit"
+    fitting_type = "Overfit"
     if test_error < train_error:
-        model = "Underfit"
+        fitting_type = "Underfit"
 
-    return model, train_error, test_error
-
-
-def split_data(data):
-    """
-    Split data into test and train data for cross validation.
-     Train Data     | Test Data
-     Set 1          | Set 2, 3, 4, 5, 6
-     Set 2          | Set 1, 3, 4, 5, 6
-     .................
-     """
-    testXY = []
-    trainXY = data
-    for i in range(len(data)):
-        test_X = []
-        test_Y = []
-        for j in range(len(data)):
-            if i != j:
-                test_X += list(data[j][0])
-                test_Y += list(data[j][1])
-        assert len(test_X) == len(test_Y)
-        testXY.append((np.asarray(test_X), np.asarray(test_Y)))
-    return trainXY, testXY
+    return fitting_type, train_error, test_error
 
 
 if __name__ == '__main__':
-    # Get the data with the following parameters
+    # Parameter defining data and the equation we're fitting
+    degree = 20  # Which degree of equation to fit the data
     N = [300, 400, 600, 1200]
-    sigma = [0.05]
-    data = generate_data(N, sigma)
-
-    coeffs = []
-    degree = 20
-    lambdas = [0, 1, 2]
-    train_data, test_data = split_data(data)
+    sigmas = [0.05]
+    lambdas = [0, 1, 2]  # reguarlization parameter
 
     # Make a list of types of data
-    properties = []
-    for num_point in N:
-        for sigma_value in sigma:
-            properties.append("N : {} Sigma : {}".format(num_point, sigma_value))
-
     color = ['black', 'green', 'gray', 'red']
 
-    for i, [X, y] in enumerate(data):
-        plt.scatter(X, y, color='black', linewidths=0.01)
-        patch = [
-            mpatches.Patch(color=color[0], label='Original'),
-            mpatches.Patch(color=color[1], label='9 Deg lambda 0'),
-            mpatches.Patch(color=color[2], label='9 Deg lambda 1'),
-            mpatches.Patch(color=color[3], label='9 Deg lambda 2')]
-        plt.legend(handles=patch)
-        plt.title('Fitting polynomial eq to {}'.format(properties[i]))
+    # Legends
+    patch = [
+        mpatches.Patch(color=color[0], label='Original'),
+        mpatches.Patch(color=color[1], label='9 Deg lambda 0'),
+        mpatches.Patch(color=color[2], label='9 Deg lambda 1'),
+        mpatches.Patch(color=color[3], label='9 Deg lambda 2')]
+    plt.legend(handles=patch)
 
-        # Fit different values of lambda
-        for index, l in enumerate(lambdas):
-            coeff, mse = ridge_regression(X, y, degree, lambda_p=l)
+    # Loop over all the types of dataset we need to fit ridge regression
+    for num_point in N:
+        for sigma in sigmas:
+            X, y = generate_data(num_point, sigma)
+            plt.scatter(X, y, color=color[0], linewidths=0.01)
 
-            coeffs.append(coeff)
-            plt.scatter(X, fit(X, coeff), c=color[index + 1], linewidths=0.1)
-            fitting, train_error, test_error = check_fit(X, y, test_data[i][0], test_data[i][1], coeff)
-            summary = ",{}, {}, {}, {}, {}, {}, {}".format(properties[i], degree, fitting, np.round(mse, 3),
-                                                           np.round(train_error, 3),
-                                                           np.round(test_error, 3), np.round(coeff, 3))
-            print(summary + "\n")
-            logging.info(summary)
+            for index, l in enumerate(lambdas):
+                data_description = "N : {} Sigma : {}".format(num_point, sigma)
+                plt.title('Fitting polynomial eq to {}'.format(data_description))
 
-        plt.show()
-        plt.pause(1)
+                coeff, mse = ridge_regression(X, y, degree, lambda_p=l)
+                plt.scatter(X, predict(X, coeff), color=color[index], linewidths=0.01)
+
+                fitting_type, train_error, test_error = cross_validate(X, y, coeff, sigma)
+                summary = ",{}, {}, {}, {}, {}, {}, {}" \
+                    .format(data_description, degree, fitting_type, np.round(mse, 3),
+                            np.round(train_error, 3), np.round(test_error, 3), np.round(coeff, 3))
+                print(summary + "\n")
+                logging.info(summary)
+
+            plt.show()
+            plt.pause(1)
