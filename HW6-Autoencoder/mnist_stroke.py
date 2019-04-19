8f3ab84fba1ce7f5ea5be7de5478e6409c11f42a
wasimusu <wasim.khan@aggiemail.usu.edu>
2019-04-19 17:46:52 -0600

    mnist stroke loader and processor done

--- HW6-Autoencoder/mnist_stroke.py
+++ HW6-Autoencoder/mnist_stroke.py
@@ -1,7 +1,7 @@
 import urllib.request
 import os
-import tarfile
 import gzip
+import tarfile
 
 import torchvision
 import torchvision.transforms as transforms
@@ -9,6 +9,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import numpy as np
 
 device = ('cuda' if torch.cuda.is_available() else 'cpu')
 batch_size = 64
@@ -20,7 +21,7 @@ delta_loss = 0.01
 reuse_model = True
 
 
-class MnistStroke:
+class MnistStrokeSequence:
     def __init__(self, train=True, shuffle=True, batch_size=1, root_dir="data"):
         """
         :param train: true if data is to be used for training
@@ -31,17 +32,61 @@ class MnistStroke:
         self.train = train
         self.shuffle = shuffle
         self.batch_size = batch_size
-        self.url_sequence = "https://github.com/edwin-de-jong/mnist-digits-stroke-sequence-data/blob/master/sequences.tar.gz"
-        self.url_digit_thinned = "https://github.com/edwin-de-jong/mnist-digits-stroke-sequence-data/blob/master/digit-images-thinned.tar.gz"
         self.root_dir = root_dir
+
+        self.url_sequence = "https://github.com/edwin-de-jong/mnist-digits-stroke-sequence-data/raw/master/sequences.tar.gz"
         self.sequence_fname = "sequences.tar.gz"
-        self.digit_fname = "digit-images-thinned.tar.gz"
+        self.sequence_fname = os.path.join(self.root_dir, self.sequence_fname)
+
+        # self.url_digit_thinned = "https://github.com/edwin-de-jong/mnist-digits-stroke-sequence-data/raw/master/digit-images-thinned.tar.gz"
+        # self.digit_fname = "digit-images-thinned.tar.gz"
+        # self.digit_fname = os.path.join(self.root_dir, self.digit_fname)
 
         self.maybe_download()
 
     def __next__(self):
-        file = tarfile.open(self.digit_fname, mode='r').read()
-        print(file)
+        self.inputs = np.load(self.processed_inputs)
+        self.labels = np.load(self.processed_labels)
+
+        return self.inputs[0], self.labels[0]
+
+    def process(self, save_dir=""):
+        self.processed_dir = "data/processed_mnist"
+        self.processed_inputs = os.path.join(self.processed_dir, "inputs.npy")
+        self.processed_labels = os.path.join(self.processed_dir, "labels.npy")
+        if not os.path.exists(self.processed_dir):
+            os.makedirs(self.processed_dir)
+        else:
+            if os.path.exists(self.processed_inputs) and os.path.exists(self.processed_labels):
+                print("Processed files already exist")
+                return
+
+        # If you have not already processed the sequence files, you can process them now
+        self.sequence_dir = "/home/wasim/Documents/sequences"
+        if not os.path.exists(self.sequence_dir):
+            raise ValueError("The file of sequences does not occur at ", self.sequence_dir)
+
+        files = os.listdir(self.sequence_dir)
+        files = [file for file in files if file.__contains__("targetdata")]
+        print("Total number of sample digits : ", len(files))
+
+        inputs, labels = [], []
+        for fname in files[:10]:
+            fname = os.path.join(self.sequence_dir, fname)
+            sequence = open(fname, mode='r', encoding='utf8').read().splitlines()
+            sequence = " ".join(sequence).split()
+            sequence = np.asarray(sequence, np.int8).reshape(-1, 14)
+            label = sequence[:, :10]
+            label = np.mean(label, axis=0)
+            assert sum(label) == 1.0
+            label = np.argsort(label.tolist())[-1]
+
+            input = sequence[:, 10:]
+            labels.append(label)
+            inputs.append(input)
+
+        np.save(self.processed_labels, labels)
+        np.save(self.processed_inputs, inputs)
 
     def next(self):
         return self.__next__()
@@ -51,21 +96,20 @@ class MnistStroke:
         if not os.path.exists(self.root_dir):
             os.makedirs(self.root_dir)
 
-        self.sequence_fname = os.path.join(self.root_dir, self.sequence_fname)
-        self.digit_fname = os.path.join(self.root_dir, self.digit_fname)
-
         if not os.path.exists(self.sequence_fname):
             print("Downloading ... ", self.sequence_fname)
             urllib.request.urlretrieve(self.url_sequence, self.sequence_fname)
+            # print("Extracting tar.gz and saving them at the same location")
+            # tarfile.open(self.sequence_fname).extractall(self.root_dir)
 
-        if not os.path.exists(self.digit_fname):
-            print("Downloading ... ", self.digit_fname)
-            urllib.request.urlretrieve(self.url_digit_thinned, self.digit_fname)
+        # if not os.path.exists(self.digit_fname):
+        #     print("Downloading ... ", self.digit_fname)
+        #     urllib.request.urlretrieve(self.url_digit_thinned, self.digit_fname)
 
 
-class MnistStrokeClassifier(nn.Module):
+class MnistStrokeSequenceClassifier(nn.Module):
     def __init__(self, input_size=10, hidden_size=64, num_layers=1, batch_size=64, bidirectional=False):
-        super(MnistStrokeClassifier, self).__init__()
+        super(MnistStrokeSequenceClassifier, self).__init__()
         self.num_layers = num_layers
         self.hidden_size = hidden_size
         self.bidirectional = bidirectional
@@ -114,61 +158,66 @@ def getAccuracy(model, dataLoader):
     return accuracy
 
 
-# Defining optimizer and criterion (loss function), optimizer and model
-model = MnistStrokeClassifier()
-optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
-criterion = nn.MSELoss()
+def train():
+    # Defining optimizer and criterion (loss function), optimizer and model
+    model = MnistStrokeSequenceClassifier()
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
+    criterion = nn.MSELoss()
 
-# Use pretrained model or train new
-if reuse_model == True:
-    if os.path.exists(filename):
-        model.load_state_dict(torch.load(f=filename))
-    else:
-        print("No pre-trained model detected. Starting fresh model training.")
-model.to(device)
+    # Use pretrained model or train new
+    if reuse_model == True:
+        if os.path.exists(filename):
+            model.load_state_dict(torch.load(f=filename))
+        else:
+            print("No pre-trained model detected. Starting fresh model training.")
+    model.to(device)
 
-# Train the model and periodically compute loss and accuracy on test set
-cur_epoch_loss = 10
-prev_epoch_loss = 20
-epoch = 1
-while abs(prev_epoch_loss - cur_epoch_loss) >= delta_loss:
-    epoch_loss = 0
-    for i, data in enumerate(trainloader):
-        inputs, labels = data
+    # Train the model and periodically compute loss and accuracy on test set
+    cur_epoch_loss = 10
+    prev_epoch_loss = 20
+    epoch = 1
+    while abs(prev_epoch_loss - cur_epoch_loss) >= delta_loss:
+        epoch_loss = 0
+        for i, data in enumerate(trainloader):
+            inputs, labels = data
 
-        inputs = inputs.to(device).squeeze(1)
-        labels = labels.to(device)
+            inputs = inputs.to(device).squeeze(1)
+            labels = labels.to(device)
 
-        if inputs.size(0) != batch_size: continue
+            if inputs.size(0) != batch_size: continue
 
-        output = model(inputs)
+            output = model(inputs)
 
-        model.zero_grad()
-        loss = criterion(output, labels)
+            model.zero_grad()
+            loss = criterion(output, labels)
 
-        loss.backward(retain_graph=True)
-        optimizer.step()
+            loss.backward(retain_graph=True)
+            optimizer.step()
 
-        epoch_loss += loss
+            epoch_loss += loss
 
-    print("{} Epoch. Loss : {}".format(epoch, "%.3f" % epoch_loss))
+        print("{} Epoch. Loss : {}".format(epoch, "%.3f" % epoch_loss))
 
-    # Every ten epochs compute validation accuracy
-    if epoch % 10 == 0:
-        print("{} Epoch. Accuracy on validation set : {}".format(epoch, "%.3f" % getAccuracy(model, validationloader)))
+        # Every ten epochs compute validation accuracy
+        if epoch % 10 == 0:
+            print("{} Epoch. Accuracy on validation set : {}".format(epoch,
+                                                                     "%.3f" % getAccuracy(model, validationloader)))
 
-    # Save the model every ten epochs
-    if epoch % 10 == 0:
-        torch.save(model.state_dict(), f=filename)
-        print()
+        # Save the model every ten epochs
+        if epoch % 10 == 0:
+            torch.save(model.state_dict(), f=filename)
+            print()
 
-    epoch += 1  # Incremenet the epoch counter
-    prev_epoch_loss = cur_epoch_loss
-    cur_epoch_loss = epoch_loss
+        epoch += 1  # Incremenet the epoch counter
+        prev_epoch_loss = cur_epoch_loss
+        cur_epoch_loss = epoch_loss
 
-# Do inference on test set
-print("Accuracy on test set : {}".format("%.4f" % getAccuracy(model, testloader)))
+    # Do inference on test set
+    print("Accuracy on test set : {}".format("%.4f" % getAccuracy(model, testloader)))
+
 
 if __name__ == '__main__':
-    train_data = MnistStroke(train=True, shuffle=True)
-    train_data.next()
+    train_data = MnistStrokeSequence(train=True, shuffle=True)
+    train_data.process()
+    inputs, labels = train_data.next()
+    print(inputs, labels)
